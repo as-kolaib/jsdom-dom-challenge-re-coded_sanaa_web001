c4bcc0f584b748bb0c6f5b659ea86a4e608ad4c8
Asma Kolaib <as.kolaib@gmail.com>
2020-08-13 13:28:29 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -8,6 +8,7 @@ let list =document.getElementById('list');
 let input =document.getElementById('comment-input');
 let submit = document.getElementById('submit');
 let form = document.getElementById('comment-form');
+let but = document.getElementsByTagName("button");
 
 
 
@@ -20,4 +21,43 @@ console.log('dfsd');
 });
 
 
+
+let timer = function (){
+    return setInterval(function(){
+        var counter = document.getElementById("counter"),
+         countInt= parseInt(counter.innerHTML);
+        counter.innerHTML = countInt + 1;
+
+    },1000);
+}
+
+let interval =timer();
+console.log(interval);
+minus.addEventListener('click',function(){
+    var counter =document.getElementById("counter"),
+     countInt= parseInt(counter.innerHTML);
+    counter.innerHTML = countInt - 1;
+});
+plus.addEventListener('click',function(){
+    var counter =document.getElementById("counter"),
+    countInt= parseInt(counter.innerHTML);
+   counter.innerHTML = countInt+ 1;
+});
+
+
+  
+var playing = true;
+pause.addEventListener("click", function () {
+    playing ? (playing = false, clearInterval(interval), this.innerText = "resume") : (playing = true, interval = timer(), this.innerText = "pause"),
+    _toConsumableArray(document.getElementsByTagName("button")).forEach(function (a) { "pause" !== a.id && (a.disabled = !playing) });
+});
+
+
+function _toConsumableArray(a) {
+  let  newArr = [];
+    if (Array.isArray(a)) 
+    { for (var b = 0; b < a.length; b++)
+        newArr[b] = a[b]; return newArr }
+    return Array.from(a)
+}
 });
